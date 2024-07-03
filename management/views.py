4ae0384eec8d3802ea04558c60c3d30d3a507a26
Gift Mugaragumbo <giftmugaragumbojr@gmail.com>
2024-07-04 00:56:46 +0200

    Added html templates for management app

--- management/views.py
+++ management/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def dashboard_view(request):
+    context = {}
+    return render(request, 'management/manager_dash.html', context)
+
+
+def player_list_view(request):
+    context = {}
+    return render(request, 'management/player_list.html', context)
+
+
+def player_profile_view(request):
+    context = {}
+    return render(request, 'management/player_profile.html', context)
+
+
+def upcoming_tournament_list_view(request):
+    context = {}
+    return render(request, 'management/upcoming_tournament_list.html', context)
+
+
+def past_tournament_list_view(request):
+    context = {}
+    return render(request, 'management/past_tournament_detail_view.html', context)
+
+
+def tournament_detail_view(request):
+    context = {}
+    return render(request, 'management/tournament_list.html', context)
+
+
+def results_list_view(request):
+    context = {}
+    return render(request, 'management/result_list.html', context)
